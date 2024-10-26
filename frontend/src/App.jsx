c2ca18aa4fb44d99688bd4b5c3749fc22459362e
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-26 15:01:58 +0530

    login register added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import {Navbar} from './components/Navbar'
 import {Login} from './components/Login'
 import {Register} from './components/Register'
+import {Home} from './components/Home'
+import { BrowserRouter } from 'react-router-dom'
+import RootPage from './components/RootPage'
+import About from './components/About'
 export default function App() {
   return (
     <>
       <Navbar/>
-      <Register/>
+      <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<RootPage/>}/>
+        <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/register"} element={<Register/>}/>
+        <Route path={"/about"} element={<About/>}/>
+        <Route path={"/Home"} element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
    </>
   )
 }
